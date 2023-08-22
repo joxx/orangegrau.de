f870b2c4df08d9882eca97f2d334cfc04d93f888
Joachim Kliemann <joachim.kliemann@googlemail.com>
2023-08-22 10:50:11 +0200

    update javascript build process

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,16 @@
+import terser from '@rollup/plugin-terser';
+
 export default {
   input: 'src/js/main.js',
-  output: {
-    file: 'public/js/bundle.js',
-    format: 'es'
-  }
+  output: [
+    {
+      file: 'public/js/bundle.js',
+      format: 'es'
+    },
+    {
+      file: 'public/js/bundle.min.js',
+      format: 'es',
+      plugins: [terser()]
+    }
+  ]
 };
